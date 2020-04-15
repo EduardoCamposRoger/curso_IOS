d67e10854996da831ac3f6df6445abf5d1bb65d0
Eduardo Campos <eduardoc@supermasymas.com>
2020-04-15 13:23:20 +0200

    Initial Commit

---
+++ T21Ej1EduardoCampos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  T21Ej1EduardoCampos
+//
+//  Created by Campos on 15/04/2020.
+//  Copyright © 2020 Hijos de Luis Rodriguez S.A. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
